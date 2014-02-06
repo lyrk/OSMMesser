22d9d6aa743cb7b55e0143192b844f24a43c7e2c
Thomas Skowron <th@skowron.biz>
2014-02-06 01:26:40 +0100

    Better command line handling

--- osmmesser.py
+++ osmmesser.py
@@ -10,7 +10,9 @@ Redistribution and use in source and binary forms, with or without modification,
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
 
+import os
 import sys
+import subprocess
 import argparse
 from math import radians, cos, sin, asin, sqrt
 
@@ -18,25 +20,29 @@ try:
 	from imposm.parser import OSMParser
 except ImportError as e:
 	print("""It appears that imposm.parser is not installed.
-In order to install it, type: pip install imposm.parser""")
+In order to install dependencies, type: python setup.py install""")
 	sys.exit(0)
 
+
 class OSMMeter(object):
-	def __init__(self, filepath):
+	def __init__(self, filepath, silent=True):
 		self.filepath = filepath
 		self.way_distances = []
 		self.coords = {}
 		self.ways = []
+		self.silent = silent
 
 
 	def calc_all_ways(self):
 		p = OSMParser(concurrency=4, coords_callback=self._read_coords,
 			ways_callback=self._read_ways)
-		print("Reading file...")
+		if not self.silent:
+			print("Reading file")
 		p.parse(self.filepath)
-		print("Summing... ")
+		if not self.silent:
+			print("Summing")
 		self._calc_ways()
-		print(sum(self.way_distances))
+		return sum(self.way_distances)
 
 
 	def _read_coords(self, coordlist):
@@ -89,13 +95,43 @@ class OSMMeter(object):
 		return EARTHRAD * c
 
 
+def filter_and_process(filepath, filterargs, silent):
+	converted = False
+
+	if filepath.endswith(".pbf"):
+		o5mfile = "osmmesser_temp.o5m"
+		if not silent:
+			print("Converting file")
+		subprocess.call("osmconvert {} -o={}".format(filepath, o5mfile), shell=True)
+		converted = True
+	else:
+		o5mfile = filepath
+
+	outputpath = "osmmesser_temp.osm"
+	if not silent:
+		print("Filtering file")
+	subprocess.call("""osmfilter {} --keep="{}" -o={}""".format(o5mfile, filterargs, outputpath), shell=True)
+	o = OSMMeter(outputpath)
+	print(str(o.calc_all_ways()) + " km")
+
+	if converted:
+		os.remove(o5mfile)
+	os.remove(outputpath)
+
+
 def main():
 	parser = argparse.ArgumentParser(description="Measuring way lengths in OSM files.")
+	parser.add_argument('--filter', help="""filtering arguments for osmfilter, as specified in http://wiki.openstreetmap.org/wiki/Osmfilter, e.g. "highway=track" """)
 	parser.add_argument('osmfile', help="file to be processed")
+	parser.add_argument('-s', '--silent', action='store_true', help='prints out only the final result, no progress will be show while processing')
 
 	args = parser.parse_args()
-	o = OSMMeter(args.osmfile)
-	o.calc_all_ways()
+
+	if args.filter:
+		filter_and_process(args.osmfile, args.filter, args.silent)
+	else:
+		o = OSMMeter(args.osmfile)
+		print(str(o.calc_all_ways()) + " km")
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import subprocess
 from setuptools import setup, find_packages
 
 setup(
@@ -5,12 +8,12 @@ setup(
 	version = "0.1",
 	scripts = ['osmmesser.py'],
 	install_requires = ['imposm.parser>=1.0.4'],
-
-	package_data = {
-	},
-
 	author = "Thomas Skowron",
 	author_email = "thomas@lyrk.org",
 	description = "A tool for measuring ways in OpenStreetMap files.",
 	license = "BSD"
 )
+
+if sys.argv[1] == "install":
+	subprocess.call("wget -O - http://m.m.i24.cc/osmfilter.c | cc -x c - -O3 -o /usr/local/bin/osmfilter", shell=True)
+	subprocess.call("wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o /usr/local/bin/osmconvert", shell=True)
